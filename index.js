631b4f3e2d09f681e1922c47d532c4f1f6bb2621
Luana Lúcia <92230804+LuanaLucia@users.noreply.github.com>
2022-11-08 20:23:39 -0300

    instalação nodemon

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Word!')
+app.get("/", (req, res) => {
+  res.send("Hello Word!");
 });
 
-app.get('/oi', (req, res) => {
-    res.send('Helo Word!')
-  });
+app.get("/oi", (req, res) => {
+  res.send("Helo Word!");
+});
+
+// listar itens
+const itens = {
+  Café: [
+    { Tipo: "Café Pelé", id: 1 },
+    { Tipo: "Café Pilão", id: 2 },
+    { Tipo: "Café Arábico", id: 3 },
+  ],
+};
+
+// endpoint  READ ALL
+app.get("/itens", (req, res) => {
+  res.send(itens);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
