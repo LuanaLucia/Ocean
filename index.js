55ed74dfddc80411d4a65a3b007b8831491bb24a
Luana Lúcia <92230804+LuanaLucia@users.noreply.github.com>
2022-11-10 20:54:26 -0300

    Atualização de endpoint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
+const url = "mongodb+srv://admin:0SFg64mqRslmbPaX@cluster0.taniag6.mongodb.net/?retryWrites=true&w=majority";
 const bancoDadosNome = "ocean_jornada_fullstack";
 
 async function main() {
@@ -16,14 +16,6 @@ async function main() {
   // Sinalizar uso de Json
   app.use(express.json());
 
-  app.get("/", (req, res) => {
-    res.send("Hello Word!");
-  });
-
-  app.get("/oi", (req, res) => {
-    res.send("Helo Word!");
-  });
-
   // listar itens
   const itens = {
     Cafe: [
@@ -34,45 +26,56 @@ async function main() {
   };
 
   // endpoint [get] READ ALL
-  app.get("/itens", (req, res) => {
-    res.send(itens.Cafe.filter(Boolean));
+  app.get("/itens", async (req, res) => {
+    const documentos = await collection.find().toArray();
+    res.send(documentos);
   });
 
   // endpoint [get] READ one
-  app.get("/itens/:id", (req, res) => {
+  app.get("/itens/:id", async (req, res) => {
     const id = req.params.id;
 
-    const item = itens.Cafe.filter(function (a) {
-      console.log(id);
-      return a.id === id;
+    const item = await collection.findOne({
+      _id: new ObjectId(id),
     });
+
     res.send(item);
   });
 
   // endpoint [put] UPDATE
-  app.put("/itens/:id", (req, res) => {
+  app.put("/itens/:id", async (req, res) => {
     const id = req.params.id;
-    itens.Cafe[id] = req.body;
-    res.send(itens);
+    const item = req.body;
+
+    await collection.updateOne({ _id: new ObjectId(id) }, { $set: item });
+
+    res.send("Item atualizado com sucesso!");
   });
 
   // endpoint [delete] DELETE
-  app.delete("/itens/:id", (req, res) => {
+  app.delete("/itens/:id", async (req, res) => {
     const id = req.params.id;
-    delete itens.Cafe[id];
-    res.send(itens);
+
+    // Remove o item do banco de dados
+    await collection.deleteOne({
+      _id: new ObjectId(id),
+    });
+
+    // Exibimos uma mensagem de sucesso
+    res.send("Item removido com sucesso!");
   });
 
   // endpoint [post]
-  app.post("/itens", (req, res) => {
+  app.post("/itens", async (req, res) => {
     const item = req.body;
-    itens.Cafe.push(item);
-    res.send(itens);
+    await collection.insertOne(item);
+    res.send("Item inserido");
   });
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
+  //0SFg64mqRslmbPaX
 }
 
 main();
