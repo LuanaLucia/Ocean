cab09956fe0fcfa50ceb90d63a40185c029b8332
LuanaLucia <luanasantos@outlook.com>
2022-11-21 21:54:44 -0300

    Header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+function Header(){
+    return( 
+    <div className="Header">
+        <a href="/">Logo</a>
+        <a href="/">Listar</a>
+        <a href="/adicionar">Adicionar</a>
+    </div>
+    )
+}
+
+export default Header;
