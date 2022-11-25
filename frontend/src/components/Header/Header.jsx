62f4850c7e70c5949f494824fbe887b1d9846d5f
LuanaLucia <luanasantos@outlook.com>
2022-11-24 21:59:54 -0300

    Ajuste <a> para <Link>

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 import "./Header.css"
 
 function Header(){
     return( 
     <div className="Header">
-        <a href="/">Logo</a>
-        <a href="/">Listar</a>
-        <a href="/adicionar">Adicionar</a>
+        <Link to="/">Logo</Link>
+        <Link to="/">Listar</Link>
+        <Link to="/adicionar">Adicionar</Link>
     </div>
     )
 }
